18ad50f53a53d383c494eae900606e4d1d5e3758
Abi-CP <c.p.abishek@gmail.com>
2024-06-07 11:23:03 +0530

    minor fixes

--- server/dist/env.js
+++ server/dist/env.js
@@ -1,3 +1,3 @@
-//export const serverUrl = "http://localhost:3000"
+export const serverUrl = "http://localhost:3000"
 // export const serverUrl = "http://192.168.228.244:3000"
- export const serverUrl  = "http://172.183.72.184:3000"
+// export const serverUrl  = "http://172.183.72.184:3000"
